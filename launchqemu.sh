4afdceae244a097f4264bc72153a293b991cb482
AntonioSposito <122155110+AntonioSposito@users.noreply.github.com>
2023-01-31 12:50:18 +0100

    Add files via upload

---
+++ launchqemu.sh
@@ -0,0 +1,2 @@
+qemu-system-aarch64 -nographic   -M virt,secure=on,virtualization=on,gic-version=3    -cpu cortex-a53 -smp 4 -m 4G   -bios ./wrkdir/imgs/qemu-aarch64-virt//flash.bin    -device loader,file="./wrkdir/imgs/qemu-aarch64-virt/baremetal/bao.bin",addr=0x50000000,force-raw=on   -device virtio-net-device,netdev=net0 -netdev user,id=net0,hostfwd=tcp:127.0.0.1:5555-:22   -device virtio-serial-device -chardev pty,id=serial3 -device virtconsole,chardev=serial3 -s
+
